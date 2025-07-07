9895e0bd231d3172adfd924fe231d8d85b5516e4
Eng-RaedMasood <rraed225588@gmail.com>
2025-07-07 08:49:14 +0300

    Update ma.dart

--- lib/ma.dart
+++ lib/ma.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 
 class D extends StatelessWidget {
   const D({super.key});
-
+var i=0;
   @override
   Widget build(BuildContext context) {
     return const Placeholder();
